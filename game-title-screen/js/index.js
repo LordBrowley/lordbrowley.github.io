a67fca19246d3c4a05ca36a6f49e3f07c4fc2114
LordBrowley <18browleybg2@webmail.sis.edu.hk>
2016-10-05 11:57:11 +0800

    yes

---
+++ game-title-screen/js/index.js
@@ -0,0 +1,16 @@
+function code() {
+  var one = prompt ("enter the stuffs");
+  if(one === "no") {
+    alert("ok")
+  }
+  else {
+    alert("go away");
+  }
+ var op = 0;
+  if (one = "no") var op = 1;
+  else var op = 2;
+  if (op = 1) {window.open("#");}
+  }
+function game() {
+  window.open("#");
+}
