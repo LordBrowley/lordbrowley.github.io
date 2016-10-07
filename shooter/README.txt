6f988887a9404a4fb3c1d7decb79f2999db5821e
LordBrowley <18browleybg2@webmail.sis.edu.hk>
2016-10-07 13:41:19 +0800

    kk

--- shooter/README.txt
+++
@@ -1,3 +0,0 @@
-A Pen created at CodePen.io. You can find this one at http://codepen.io/LordBrowley/pen/yapQzz.
-
- cannon.js and three.js, updated for three r77
--- title/js/index.js
+++ title/js/index.js
@@ -1,16 +1,4 @@
-function code() {
-  var one = prompt ("enter the stuffs");
-  if(one === "no") {
-    alert("ok")
-  }
-  else {
-    alert("go away");
-  }
- var op = 0;
-  if (one = "no") var op = 1;
-  else var op = 2;
-  if (op = 1) {window.open("https://lordbrowley.github.io/shooter/");}
-  }
+
 function game() {
   window.open("https://lordbrowley.github.io/platformer/");
 }
