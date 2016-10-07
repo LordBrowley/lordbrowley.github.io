a37c953ea69de67a3e4fa3807a43cf8859685016
LordBrowley <18browleybg2@webmail.sis.edu.hk>
2016-10-07 13:35:33 +0800

    vty

---
+++ title/js/index.js
@@ -0,0 +1,16 @@
+function code() {
+  var one = prompt ("enter the stuffs");
+  if(one === "no") {
+    alert("ok")
+  }
+  else {
+    alert("go away");
+  }
+ var op = 0;
+  if (one = "no") var op = 1;
+  else var op = 2;
+  if (op = 1) {window.open("https://lordbrowley.github.io/shooter/");}
+  }
+function game() {
+  window.open("https://lordbrowley.github.io/platformer/");
+}
